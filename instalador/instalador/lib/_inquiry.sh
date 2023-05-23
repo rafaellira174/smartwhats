d2d850a009d0c8ca691bf28d19f43ea4c4a41306
rafaellira174 <124856521+rafaellira174@users.noreply.github.com>
2023-05-23 19:07:39 -0300

    Add files via upload

---
+++ instalador/instalador/lib/_inquiry.sh
@@ -0,0 +1,241 @@
+#!/bin/bash
+
+get_mysql_root_password() {
+  
+  print_banner
+  printf "${WHITE} 💻 Insira senha para o usuario Deploy e Banco de Dados (Não utilizar caracteres especiais):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " mysql_root_password
+}
+
+get_link_git() {
+  
+  print_banner
+  printf "${WHITE} 💻 Insira o link GITHUB do SmartAtendimento:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " link_git
+}
+
+get_instancia_add() {
+  
+  print_banner
+  printf "${WHITE} 💻 Informe um nome para a Instancia (Não utilizar espaços ou caracteres especiais, Utilizar Letras minusculas; ):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " instancia_add
+}
+
+get_max_whats() {
+  
+  print_banner
+  printf "${WHITE} 💻 Informe a Qtde de Conexões/Whats que a ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " max_whats
+}
+
+get_max_user() {
+  
+  print_banner
+  printf "${WHITE} 💻 Informe a Qtde de Usuarios/Atendentes que a ${instancia_add} poderá cadastrar:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " max_user
+}
+
+get_frontend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o FRONTEND para ${instancia_add}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " frontend_url
+}
+
+get_backend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o BACKENDI para ${instancia_add}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " backend_url
+}
+
+get_frontend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do FRONTEND para ${instancia_add}; Ex: 3000 A 3999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " frontend_port
+}
+
+
+get_backend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do BACKEND para esta instancia; Ex: 4000 A 4999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " backend_port
+}
+
+get_redis_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do REDIS/AGENDAMENTO MSG para ${instancia_add}; Ex: 5000 A 5999 ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " redis_port
+}
+
+get_empresa_delete() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da Instancia que será Deletada (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_delete
+}
+
+get_empresa_atualizar() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da Instancia que deseja Atualizar (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_atualizar
+}
+
+get_empresa_bloquear() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da Instancia que deseja Bloquear (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_bloquear
+}
+
+get_empresa_desbloquear() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da Instancia que deseja Desbloquear (Digite o mesmo nome de quando instalou):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_desbloquear
+}
+
+get_empresa_dominio() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o nome da Instancia que deseja Alterar os Dominios (Atenção para alterar os dominios precisa digitar os 2, mesmo que vá alterar apenas 1):${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " empresa_dominio
+}
+
+get_alter_frontend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o NOVO domínio do FRONTEND para ${empresa_dominio}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_frontend_url
+}
+
+get_alter_backend_url() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite o NOVO domínio do BACKEND para ${empresa_dominio}:${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_backend_url
+}
+
+get_alter_frontend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do FRONTEND da Instancia ${empresa_dominio}; A porta deve ser o mesma informada durante a instalação ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_frontend_port
+}
+
+
+get_alter_backend_port() {
+  
+  print_banner
+  printf "${WHITE} 💻 Digite a porta do BACKEND da Instancia ${empresa_dominio}; A porta deve ser o mesma informada durante a instalação ${GRAY_LIGHT}"
+  printf "\n\n"
+  read -p "> " alter_backend_port
+}
+
+
+get_urls() {
+  get_mysql_root_password
+  get_link_git
+  get_instancia_add
+  get_max_whats
+  get_max_user
+  get_frontend_url
+  get_backend_url
+  get_frontend_port
+  get_backend_port
+  get_redis_port
+}
+
+software_update() {
+  get_empresa_atualizar
+  frontend_update
+  backend_update
+}
+
+software_delete() {
+  get_empresa_delete
+  deletar_tudo
+}
+
+software_bloquear() {
+  get_empresa_bloquear
+  configurar_bloqueio
+}
+
+software_desbloquear() {
+  get_empresa_desbloquear
+  configurar_desbloqueio
+}
+
+software_dominio() {
+  get_empresa_dominio
+  get_alter_frontend_url
+  get_alter_backend_url
+  get_alter_frontend_port
+  get_alter_backend_port
+  configurar_dominio
+}
+
+inquiry_options() {
+  
+  print_banner
+  printf "${WHITE} 💻 Bem vindo(a) ao Gerenciador SmartAtendimento, selecione abaixo a proxima ação!${GRAY_LIGHT}"
+  printf "\n\n"
+  printf "   [0] Instalar\n"
+  printf "   [1] Atualizar\n"
+  printf "   [2] Deletar\n"
+    printf "\n"
+  read -p "> " option
+
+  case "${option}" in
+    0) get_urls ;;
+
+    1) 
+      software_update 
+      exit
+      ;;
+
+    2) 
+      software_delete 
+      exit
+      ;;
+    3) 
+      software_bloquear 
+      exit
+      ;;
+    4) 
+      software_desbloquear 
+      exit
+      ;;
+    5) 
+      software_dominio 
+      exit
+      ;;        
+
+    *) exit ;;
+  esac
+}
+
+
